410e319b9217216fcee191bde875af2118227770
sedclerk <sedclerk@gmail.com>
2014-02-05 14:47:22 +0300

    create new repository

---
+++ sample_app.rb
@@ -0,0 +1,416 @@
+
+Rubis:) cd /Users/Sedclerk/Documents/car_project
+Rubis:) rails new sample_app -d mysql --skip-test-unit
+create
+create  README.rdoc
+create  Rakefile
+create  config.ru
+create  .gitignore
+create  Gemfile
+create  app
+create  app/assets/javascripts/application.js
+create  app/assets/stylesheets/application.css
+create  app/controllers/application_controller.rb
+create  app/helpers/application_helper.rb
+create  app/views/layouts/application.html.erb
+create  app/assets/images/.keep
+create  app/mailers/.keep
+create  app/models/.keep
+create  app/controllers/concerns/.keep
+create  app/models/concerns/.keep
+create  bin
+create  bin/bundle
+create  bin/rails
+create  bin/rake
+create  config
+create  config/routes.rb
+create  config/application.rb
+create  config/environment.rb
+create  config/environments
+create  config/environments/development.rb
+create  config/environments/production.rb
+create  config/environments/test.rb
+create  config/initializers
+create  config/initializers/backtrace_silencers.rb
+create  config/initializers/filter_parameter_logging.rb
+create  config/initializers/inflections.rb
+create  config/initializers/mime_types.rb
+create  config/initializers/secret_token.rb
+create  config/initializers/session_store.rb
+create  config/initializers/wrap_parameters.rb
+create  config/locales
+create  config/locales/en.yml
+create  config/boot.rb
+create  config/database.yml
+create  db
+create  db/seeds.rb
+create  lib
+create  lib/tasks
+create  lib/tasks/.keep
+create  lib/assets
+create  lib/assets/.keep
+create  log
+create  log/.keep
+create  public
+create  public/404.html
+create  public/422.html
+create  public/500.html
+create  public/favicon.ico
+create  public/robots.txt
+create  tmp/cache
+create  tmp/cache/assets
+create  vendor/assets/javascripts
+create  vendor/assets/javascripts/.keep
+create  vendor/assets/stylesheets
+create  vendor/assets/stylesheets/.keep
+run  bundle install
+Fetching gem metadata from https://rubygems.org/...........
+Fetching additional metadata from https://rubygems.org/..
+Resolving dependencies...
+Using rake (10.1.1)
+Using i18n (0.6.9)
+Using minitest (4.7.5)
+Using multi_json (1.8.4)
+Using atomic (1.1.14)
+Using thread_safe (0.1.3)
+Using tzinfo (0.3.38)
+Using activesupport (4.0.2)
+Using builder (3.1.4)
+Using erubis (2.7.0)
+Using rack (1.5.2)
+Using rack-test (0.6.2)
+Using actionpack (4.0.2)
+Using mime-types (1.25.1)
+Using polyglot (0.3.3)
+Using treetop (1.4.15)
+Using mail (2.5.4)
+Using actionmailer (4.0.2)
+Using activemodel (4.0.2)
+Using activerecord-deprecated_finders (1.0.3)
+Using arel (4.0.1)
+Using activerecord (4.0.2)
+Using bundler (1.5.2)
+Using coffee-script-source (1.7.0)
+Using execjs (2.0.2)
+Using coffee-script (2.2.0)
+Using thor (0.18.1)
+Using railties (4.0.2)
+Using coffee-rails (4.0.1)
+Using hike (1.2.3)
+Using jbuilder (1.5.3)
+Using jquery-rails (3.1.0)
+Using json (1.8.1)
+Using mysql2 (0.3.15)
+Using tilt (1.4.1)
+Using sprockets (2.10.1)
+Using sprockets-rails (2.0.1)
+Using rails (4.0.2)
+Using rdoc (4.1.1)
+Using sass (3.2.14)
+Using sass-rails (4.0.1)
+Using sdoc (0.4.0)
+Using turbolinks (2.2.1)
+Using uglifier (2.4.0)
+Your bundle is complete!
+Use `bundle show [gemname]` to see where a bundled gem is installed.
+Rubis:) bundle install --without production
+Bundler::GemfileNotFound
+Rubis:) cd sample_app
+Rubis:) bundle install --without production
+Resolving dependencies...
+Using rake (10.1.1)
+Using i18n (0.6.9)
+Using minitest (4.7.5)
+Using multi_json (1.8.4)
+Using atomic (1.1.14)
+Using thread_safe (0.1.3)
+Using tzinfo (0.3.38)
+Using activesupport (4.0.2)
+Using builder (3.1.4)
+Using erubis (2.7.0)
+Using rack (1.5.2)
+Using rack-test (0.6.2)
+Using actionpack (4.0.2)
+Using mime-types (1.25.1)
+Using polyglot (0.3.3)
+Using treetop (1.4.15)
+Using mail (2.5.4)
+Using actionmailer (4.0.2)
+Using activemodel (4.0.2)
+Using activerecord-deprecated_finders (1.0.3)
+Using arel (4.0.1)
+Using activerecord (4.0.2)
+Using bundler (1.5.2)
+Using coffee-script-source (1.7.0)
+Using execjs (2.0.2)
+Using coffee-script (2.2.0)
+Using thor (0.18.1)
+Using railties (4.0.2)
+Using coffee-rails (4.0.1)
+Using hike (1.2.3)
+Using jbuilder (1.5.3)
+Using jquery-rails (3.1.0)
+Using json (1.8.1)
+Using mysql2 (0.3.15)
+Using tilt (1.4.1)
+Using sprockets (2.10.1)
+Using sprockets-rails (2.0.1)
+Using rails (4.0.2)
+Using rdoc (4.1.1)
+Using sass (3.2.14)
+Using sass-rails (4.0.1)
+Using sdoc (0.4.0)
+Using turbolinks (2.2.1)
+Using uglifier (2.4.0)
+Your bundle is complete!
+Gems in the group production were not installed.
+Use `bundle show [gemname]` to see where a bundled gem is installed.
+Rubis:) bundle update
+Fetching gem metadata from https://rubygems.org/...........
+Fetching additional metadata from https://rubygems.org/..
+Resolving dependencies...
+Using rake (10.1.1)
+Using i18n (0.6.9)
+Using minitest (4.7.5)
+Using multi_json (1.8.4)
+Using atomic (1.1.14)
+Using thread_safe (0.1.3)
+Using tzinfo (0.3.38)
+Using activesupport (4.0.2)
+Using builder (3.1.4)
+Using erubis (2.7.0)
+Using rack (1.5.2)
+Using rack-test (0.6.2)
+Using actionpack (4.0.2)
+Using mime-types (1.25.1)
+Using polyglot (0.3.3)
+Using treetop (1.4.15)
+Using mail (2.5.4)
+Using actionmailer (4.0.2)
+Using activemodel (4.0.2)
+Using activerecord-deprecated_finders (1.0.3)
+Using arel (4.0.1)
+Using activerecord (4.0.2)
+Using bundler (1.5.2)
+Using coffee-script-source (1.7.0)
+Using execjs (2.0.2)
+Using coffee-script (2.2.0)
+Using thor (0.18.1)
+Using railties (4.0.2)
+Using coffee-rails (4.0.1)
+Using hike (1.2.3)
+Using jbuilder (1.5.3)
+Using jquery-rails (3.1.0)
+Using json (1.8.1)
+Using mysql2 (0.3.15)
+Using tilt (1.4.1)
+Using sprockets (2.10.1)
+Using sprockets-rails (2.0.1)
+Using rails (4.0.2)
+Using rdoc (4.1.1)
+Using sass (3.2.14)
+Using sass-rails (4.0.1)
+Using sdoc (0.4.0)
+Using turbolinks (2.2.1)
+Using uglifier (2.4.0)
+Your bundle is updated!
+Gems in the group production were not installed.
+Rubis:) bundle install
+Using rake (10.1.1)
+Using i18n (0.6.9)
+Using minitest (4.7.5)
+Using multi_json (1.8.4)
+Using atomic (1.1.14)
+Using thread_safe (0.1.3)
+Using tzinfo (0.3.38)
+Using activesupport (4.0.2)
+Using builder (3.1.4)
+Using erubis (2.7.0)
+Using rack (1.5.2)
+Using rack-test (0.6.2)
+Using actionpack (4.0.2)
+Using mime-types (1.25.1)
+Using polyglot (0.3.3)
+Using treetop (1.4.15)
+Using mail (2.5.4)
+Using actionmailer (4.0.2)
+Using activemodel (4.0.2)
+Using activerecord-deprecated_finders (1.0.3)
+Using arel (4.0.1)
+Using activerecord (4.0.2)
+Using coffee-script-source (1.7.0)
+Using execjs (2.0.2)
+Using coffee-script (2.2.0)
+Using thor (0.18.1)
+Using railties (4.0.2)
+Using coffee-rails (4.0.1)
+Using hike (1.2.3)
+Using jbuilder (1.5.3)
+Using jquery-rails (3.1.0)
+Using json (1.8.1)
+Using mysql2 (0.3.15)
+Using bundler (1.5.2)
+Using tilt (1.4.1)
+Using sprockets (2.10.1)
+Using sprockets-rails (2.0.1)
+Using rails (4.0.2)
+Using rdoc (4.1.1)
+Using sass (3.2.14)
+Using sass-rails (4.0.1)
+Using sdoc (0.4.0)
+Using turbolinks (2.2.1)
+Using uglifier (2.4.0)
+Your bundle is complete!
+Gems in the group production were not installed.
+Use `bundle show [gemname]` to see where a bundled gem is installed.
+
+Rubis:) git add .
+Rubis:) git commit -m "Initial commit"
+[master (root-commit) f7603fc] Initial commit
+35 files changed, 856 insertions(+)
+create mode 100644 .gitignore
+create mode 100644 Gemfile
+create mode 100644 Gemfile.lock
+create mode 100644 README.rdoc
+create mode 100644 Rakefile
+create mode 100644 app/assets/images/.keep
+create mode 100644 app/assets/javascripts/application.js
+create mode 100644 app/assets/stylesheets/application.css
+create mode 100644 app/controllers/application_controller.rb
+create mode 100644 app/controllers/concerns/.keep
+create mode 100644 app/helpers/application_helper.rb
+create mode 100644 app/mailers/.keep
+create mode 100644 app/models/.keep
+create mode 100644 app/models/concerns/.keep
+create mode 100644 app/views/layouts/application.html.erb
+create mode 100755 bin/bundle
+create mode 100755 bin/rails
+create mode 100755 bin/rake
+create mode 100644 config.ru
+create mode 100644 config/application.rb
+create mode 100644 config/boot.rb
+create mode 100644 config/database.yml
+create mode 100644 config/environment.rb
+create mode 100644 config/environments/development.rb
+create mode 100644 config/environments/production.rb
+create mode 100644 config/environments/test.rb
+create mode 100644 config/initializers/backtrace_silencers.rb
+create mode 100644 config/initializers/filter_parameter_logging.rb
+create mode 100644 config/initializers/inflections.rb
+create mode 100644 config/initializers/mime_types.rb
+create mode 100644 config/initializers/secret_token.rb
+create mode 100644 config/initializers/session_store.rb
+create mode 100644 config/initializers/wrap_parameters.rb
+create mode 100644 config/locales/en.yml
+create mode 100644 config/routes.rb
+create mode 100644 db/seeds.rb
+create mode 100644 lib/assets/.keep
+create mode 100644 lib/tasks/.keep
+create mode 100644 log/.keep
+create mode 100644 public/404.html
+create mode 100644 public/422.html
+create mode 100644 public/500.html
+create mode 100644 public/favicon.ico
+create mode 100644 public/robots.txt
+create mode 100644 vendor/assets/javascripts/.keep
+create mode 100644 vendor/assets/stylesheets/.keep
+Rubis:) rails generate rspec:install
+Could not find generator rspec:install.
+
+Last login: Tue Feb  4 07:24:02 on console
+Rubis:) cd /Users/Sedclerk/Documents/car_project/sample_app
+Rubis:) rails generate rspec:install
+Could not find generator rspec:install.
+Rubis:) cd ..
+Rubis:) rails generate rspec:install
+Usage:
+rails new APP_PATH [options]
+
+Options:
+-r, [--ruby=PATH]              # Path to the Ruby binary of your choice
+# Default: /Users/Sedclerk/.rbenv/versions/1.9.3-p392/bin/ruby
+-m, [--template=TEMPLATE]      # Path to some application template (can be a filesystem path or URL)
+[--skip-gemfile]           # Don't create a Gemfile
+-B, [--skip-bundle]            # Don't run bundle install
+-G, [--skip-git]               # Skip .gitignore file
+[--skip-keeps]             # Skip source control .keep files
+-O, [--skip-active-record]     # Skip Active Record files
+-S, [--skip-sprockets]         # Skip Sprockets files
+-d, [--database=DATABASE]      # Preconfigure for selected database (options: mysql/oracle/postgresql/sqlite3/frontbase/ibm_db/sqlserver/jdbcmysql/jdbcsqlite3/jdbcpostgresql/jdbc)
+# Default: sqlite3
+-j, [--javascript=JAVASCRIPT]  # Preconfigure for selected JavaScript library
+# Default: jquery
+-J, [--skip-javascript]        # Skip JavaScript files
+[--dev]                    # Setup the application with Gemfile pointing to your Rails checkout
+[--edge]                   # Setup the application with Gemfile pointing to Rails repository
+-T, [--skip-test-unit]         # Skip Test::Unit files
+[--rc=RC]                  # Path to file containing extra configuration options for rails command
+[--no-rc]                  # Skip loading of extra configuration options from .railsrc file
+
+Runtime options:
+-f, [--force]    # Overwrite files that already exist
+-p, [--pretend]  # Run but do not make any changes
+-q, [--quiet]    # Suppress status output
+-s, [--skip]     # Skip files that already exist
+
+Rails options:
+-h, [--help]     # Show this help message and quit
+-v, [--version]  # Show Rails version number and quit
+
+Description:
+The 'rails new' command creates a new Rails application with a default
+directory structure and configuration at the path you specify.
+
+You can specify extra command-line arguments to be used every time
+'rails new' runs in the .railsrc configuration file in your home directory.
+
+Note that the arguments specified in the .railsrc file don't affect the
+defaults values shown above in this help message.
+
+Example:
+rails new ~/Code/Ruby/weblog
+
+This generates a skeletal Rails installation in ~/Code/Ruby/weblog.
+See the README in the newly created application to get going.
+Rubis:) cd sample_app
+Rubis:) git remote add origin https://github.com/<username>/sample_app.git
+-bash: username: No such file or directory
+Rubis:) heroku create
+Creating still-sands-2374... done, stack is cedar
+http://still-sands-2374.herokuapp.com/ | git@heroku.com:still-sands-2374.git
+Rubis:) git push heroku master
+Permission denied (publickey).
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+Rubis:) heroku run rake db:migrate
+Running `rake db:migrate` attached to terminal... up, run.9356
+rake aborted!
+No Rakefile found (looking for: rakefile, Rakefile, rakefile.rb, Rakefile.rb)
+/usr/local/lib/ruby/1.9.1/rake.rb:2367:in `raw_load_rakefile'
+/usr/local/lib/ruby/1.9.1/rake.rb:2007:in `block in load_rakefile'
+/usr/local/lib/ruby/1.9.1/rake.rb:2058:in `standard_exception_handling'
+/usr/local/lib/ruby/1.9.1/rake.rb:2006:in `load_rakefile'
+/usr/local/lib/ruby/1.9.1/rake.rb:1991:in `run'
+/usr/local/bin/rake:31:in `<main>'
+Rubis:) subl .
+-bash: subl: command not found
+Rubis:) rails generate controller StaticPages home help --no-test-framework
+create  app/controllers/static_pages_controller.rb
+route  get "static_pages/help"
+route  get "static_pages/home"
+invoke  erb
+create    app/views/static_pages
+create    app/views/static_pages/home.html.erb
+create    app/views/static_pages/help.html.erb
+invoke  helper
+create    app/helpers/static_pages_helper.rb
+invoke  assets
+invoke    coffee
+create      app/assets/javascripts/static_pages.js.coffee
+invoke    scss
+create      app/assets/stylesheets/static_pages.css.scss
+Rubis:)
+Rubis:) rails generate integration_test static_pages
+Rubis:)
